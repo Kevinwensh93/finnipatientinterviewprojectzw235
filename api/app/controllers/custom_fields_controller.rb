0c07c12b78a6460e9e89fc3fae218e8718cd195d
Kevinwensh93 <99685865+Kevinwensh93@users.noreply.github.com>
2024-04-22 05:21:18 -0400

    draft 1 completed

--- api/app/controllers/custom_fields_controller.rb
+++ api/app/controllers/custom_fields_controller.rb
@@ -1,29 +1,51 @@
 class CustomFieldsController < ApplicationController
     before_action :set_patient
+    before_action :set_custom_field, only: [:show, :update, :destroy]
 
+    # GET /patients/:patient_id/custom_fields
     def index
         @custom_fields = @patient.custom_fields
         render json: @custom_fields
     end
 
-    def new
-        @custom_field = @patient.custom_fields.build
+    # GET /patients/:patient_id/custom_fields/:id
+    def show
+        render json: @custom_field
     end
 
+    # POST /patients/:patient_id/custom_fields
     def create
         @custom_field = @patient.custom_fields.build(custom_field_params)
-
         if @custom_field.save
-            redirect_to patient_custom_fields_path(@patient), notice: 'Custom field was successfully created.'
+            render json: @custom_field, status: :created, location: [@patient, @custom_field]
         else
-            render json: { errors: @custom_field.errors.full_messages }, status: :unprocessable_entity
+            render json: @custom_field.errors, status: :unprocessable_entity
         end
     end
 
-    private
+    # PATCH/PUT /patients/:patient_id/custom_fields/:id
+    def update
+        if @custom_field.update(custom_field_params)
+            render json: @custom_field
+        else
+            render json: @custom_field.errors, status: :unprocessable_entity
+        end
+    end
 
+    # DELETE /patients/:patient_id/custom_fields/:id
+    def destroy
+        @custom_field.destroy
+        head :no_content
+    end
+
+    private
+        
     def set_patient
         @patient = Patient.find(params[:patient_id])
+    end
+
+    def set_custom_field
+        @custom_field = @patient.custom_fields.find(params[:id])
     end
 
     def custom_field_params
