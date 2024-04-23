da99cc867a9ea7c793f86f691c614aea24c47fa3
Kevinwensh93 <99685865+Kevinwensh93@users.noreply.github.com>
2024-04-23 13:03:42 -0400

    UI polish

--- web_app/patient-data-form/src/App.js
+++ web_app/patient-data-form/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>React Patient Table</p>
+        <p>Finni</p>
       </header>
       <div className="content">
         <PatientTable/>
--- web_app/patient-data-form/src/patient-tables/PatientTable.js
+++ web_app/patient-data-form/src/patient-tables/PatientTable.js
@@ -190,22 +190,22 @@ function PatientTable() {
                             {expandedRow === patient.id && (
                                 <>
                                     <tr>
-                                        <td colSpan="3">
-                                            <h4>Address Information</h4>
+                                        <td colSpan="6">
+                                            <b>Address Information</b>
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td colSpan="3">
+                                        <td colSpan="6">
                                             <PatientAddressTable patient={patient} />
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td colSpan="2">
-                                            <h4>Custom Fields</h4>
+                                        <td colSpan="3">
+                                            <b>Additional information</b>
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td colSpan="2">
+                                        <td colSpan="3">
                                             <PatientCustomFieldTable patient={patient} />
                                         </td>
                                     </tr>
