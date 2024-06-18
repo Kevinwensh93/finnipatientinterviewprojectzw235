c9501a61506d4d5efc60dfb648bc9510da80d807
Kevinwensh93 <99685865+Kevinwensh93@users.noreply.github.com>
2024-06-18 15:53:49 -0400

    bugfix for cascade delete

--- api/app/models/patient.rb
+++ api/app/models/patient.rb
@@ -1,5 +1,5 @@
 class Patient < ApplicationRecord
-    has_many :addresses
-    has_many :custom_fields
+    has_many :addresses, dependent: :destroy
+    has_many :custom_fields, dependent: :destroy
     enum status: { inquiry: 0, onboarding: 1, active: 2, churned: 3 }
 end
